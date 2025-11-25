20871b8d5312ecc1cb61c78b2b88fdd2a4f9922a
hyeok90 <yujinhyeok01@naver.com>
2025-11-25 14:39:47 +0900

    Fix: Definitively correct python import paths for worker

--- worker/worker.py
+++ worker/worker.py
@@ -9,8 +9,8 @@ from typing import List
 import gc
 import sys
 
-# Add the 'vggt' sub-directory to the path to allow finding its modules
-sys.path.append("vggt")
+# Add project root to path to allow finding the 'vggt' package
+sys.path.append(".")
 
 # --- VGGT and ML Imports ---
 import cv2
@@ -19,11 +19,10 @@ import numpy as np
 import open3d as o3d
 
 # These imports must come after the sys.path modification
-from visual_util import predictions_to_glb
-from vggt.models.vggt import VGGT
-from vggt.utils.load_fn import load_and_preprocess_images
-from vggt.utils.pose_enc import pose_encoding_to_extri_intri
-from vggt.utils.geometry import unproject_depth_map_to_point_map
+from vggt.vggt.models.vggt import VGGT
+from vggt.vggt.utils.load_fn import load_and_preprocess_images
+from vggt.vggt.utils.pose_enc import pose_encoding_to_extri_intri
+from vggt.vggt.utils.geometry import unproject_depth_map_to_point_map
 
 # --- Global model instance ---
 device = "cuda" if torch.cuda.is_available() else "cpu"
